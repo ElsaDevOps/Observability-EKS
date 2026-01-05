4081257189a0e6fb1badef247d95873cfb7f6765
ElsaDevOps <inatelsa@mail.example.com>
2026-01-05 17:56:37 +0000

    added prometheus metrics

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,43 @@
 package main
 
 import (
-	"net/http"
+	
 	"log"
-	"io"
+	"net/http"
+	"github.com/prometheus/client_golang/prometheus"
     "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+type metrics struct {
+	api_up *prometheus.GaugeVec
+}
+
+
+
+func NewMetrics( reg prometheus.Registerer) *metrics {
+	m := &metrics{
+		api_up: prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "api_up_info",
+			Help: "Checks API health.",
+			
+		},
+		 []string{"provider"}),
+	}
+		
+	
+	reg.MustRegister(m.api_up)
+	return m
+}
+
+
+
 
 func main() {
-	h1 := func(w http.ResponseWriter, r *http.Request){
-		io.WriteString (w, "hello")
-
-	}
-
-	http.HandleFunc("/metrics", h1)
-	http.Handle("/metrics", promhttp.Handler())
-
+	reg := prometheus.NewRegistry()
+	m := NewMetrics(reg)
+	m.api_up.WithLabelValues("headscale").Set(1)
+    m.api_up.WithLabelValues("tailscale").Set(0)
+    http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{Registry: reg}))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
