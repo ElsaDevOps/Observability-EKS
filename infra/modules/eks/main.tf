44b4cbff68d26cd19b5d20d46a689f7ef43560ef
ElsaDevOps <elsaadjei55@gmail.com>
2026-02-13 15:01:09 +0000

    completed main.tf for EKS

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -10,7 +10,7 @@ resource "aws_eks_cluster" "main" {
   role_arn = aws_iam_role.cluster.arn
   version  = "1.31"
 
- vpc_config {
+  vpc_config {
     endpoint_private_access = true
     endpoint_public_access  = true
 
@@ -20,7 +20,7 @@ resource "aws_eks_cluster" "main" {
 
   encryption_config {
     provider {
-      key_arn = aws_kms_key.my_key.arn
+      key_arn = aws_kms_key.encrypt_key.arn
     }
     resources = ["secrets"]
   }
@@ -33,10 +33,10 @@ resource "aws_eks_cluster" "main" {
     "scheduler"
   ]
 
-  depends_on = [ aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy, ]
+  depends_on = [aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy, ]
 }
 
-resource "aws_kms_key" "my_key" {
+resource "aws_kms_key" "encrypt_key" {
   # checkov:skip=CKV2_AWS_64: Default key policy grants root account access, IAM policies control usage
   description             = "A symmetric encryption KMS key"
   enable_key_rotation     = true
@@ -65,4 +65,75 @@ resource "aws_iam_role" "cluster" {
 resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.cluster.name
+}
+
+
+resource "aws_iam_role" "node_group" {
+  name = "${var.cluster_name}-node-group-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+}
+
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node_group.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node_group.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryPullOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
+  role       = aws_iam_role.node_group.name
+}
+
+data "tls_certificate" "main" {
+  url = aws_eks_cluster.main.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  url             = aws_eks_cluster.main.identity[0].oidc[0].issuer
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.main.certificates[0].sha1_fingerprint]
+}
+
+
+
+resource "aws_eks_node_group" "main" {
+  cluster_name    = var.cluster_name
+  node_group_name = "${var.cluster_name}-node-group"
+  node_role_arn   = aws_iam_role.node_group.arn
+  subnet_ids      = var.subnet_ids
+
+  instance_types = var.node_instance_types
+
+  scaling_config {
+    desired_size = var.node_desired_size
+    min_size     = var.node_min_size
+    max_size     = var.node_max_size
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node_AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryPullOnly,
+
+  ]
 }
