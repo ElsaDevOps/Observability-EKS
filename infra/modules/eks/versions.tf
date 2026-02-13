5bab5affad45d0c4428bce6c1614c9ffadfaff58
ElsaDevOps <elsaadjei55@gmail.com>
2026-02-13 15:59:22 +0000

    added versions.tf to eks module

---
+++ infra/modules/eks/versions.tf
@@ -0,0 +1,15 @@
+
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.27.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.2.1"
+    }
+  }
+}
