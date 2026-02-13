2578bb016b1bdb0b0d823f1d92783d936b1be52b
ElsaDevOps <elsaadjei55@gmail.com>
2026-02-13 15:53:04 +0000

    completed outputs.tf for eks module

---
+++ infra/modules/eks/outputs.tf
@@ -0,0 +1,19 @@
+output "cluster_name" {
+  description = "the name of the cluster"
+  value       = aws_eks_cluster.main.name
+}
+
+output "cluster_endpoint" {
+  description = "The cluster endpoint"
+  value       = aws_eks_cluster.main.endpoint
+}
+
+output "oidc_provider_arn" {
+  description = "The ARN for the OIDC provider"
+  value       = aws_iam_openid_connect_provider.eks.arn
+}
+
+output "oidc_issuer_url" {
+  description = "The issuer URL from OIDC"
+  value       = aws_iam_openid_connect_provider.eks.url
+}
