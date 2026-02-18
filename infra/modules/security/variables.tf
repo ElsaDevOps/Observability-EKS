3cb64e63bbc3dc3c52fdb5502a08b275d285107d
ElsaDevOps <elsaadjei55@gmail.com>
2026-02-18 13:53:19 +0000

    added variables.tf for security module

---
+++ infra/modules/security/variables.tf
@@ -0,0 +1,29 @@
+variable "oidc_arn" {
+  type        = string
+  description = "The OIDC ARN"
+}
+
+variable "issuer_url" {
+  type        = string
+  description = "The OIDC issuer URL"
+}
+
+variable "role_name" {
+  type        = string
+  description = "The name of the IRSA role"
+}
+
+variable "policy_json" {
+  type        = string
+  description = "The permissions policy for roles"
+}
+
+variable "namespace" {
+  type        = string
+  description = "The namespace for the roles"
+}
+
+variable "service_account_name" {
+  type        = string
+  description = "The name of the ServiceAccount for each role"
+}
