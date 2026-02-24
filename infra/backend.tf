ea5a5f8bf544188f34dd946d3e1b3c4cb56d2241
ElsaDevOps <elsaadjei55@gmail.com>
2026-02-24 19:59:51 +0000

    added remote backend

---
+++ infra/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "terraform-state-kay8s"
+    key          = "infra/terraform.tfstate"
+    region       = "eu-west-2"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
