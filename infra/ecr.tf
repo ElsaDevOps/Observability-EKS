e6a4f4870a623bd03b7d87150216b6517be47fdd
ElsaDevOps <elsaadjei55@gmail.com>
2026-02-28 16:26:44 +0000

    working on argocd

---
+++ infra/ecr.tf
@@ -0,0 +1,9 @@
+resource "aws_ecr_repository" "my_exporter" {
+  #checkov:skip=CKV_AWS_136:Non-production environment, default AES-256 encryption sufficient
+  name                 = "my-exporter"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
